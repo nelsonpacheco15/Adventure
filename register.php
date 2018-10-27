c1991e6d24b3cd65e424c96da71d8ba83858ad2e
Cactusforlife <Miguelsardinhapimentel@hotmail.com>
2018-10-27 19:22:49 +0000

    added comments on what each thing does

--- register.php
+++ register.php
@@ -1,26 +1,32 @@
 <?php
-
+//incluir o ficheiro onde contem a ligação
 include('ligar_bd.php');
-
+//se houver um pedido de resposta do formulario(isto ta defenido no input submit, o submit faz um pedido ao servidor, que tem como nome "register")
 if($_POST['register'])
 {
+    //guardamos os valores enviados atraves do input e guardamos em variaves, isto defenimos no "name" do input
 
     $user = $_POST['user'];
     
     $name = $_POST['name'];
     
     $pass = $_POST['repeatpass'];
-    
+
+    //faz um hash da password
     $hashed_pass = crypt($pass,"123");
     
+    //cross-site scripting protection
     $name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
-    
+
+    //preparamos a query que vai ser enviada para a base de dados, onde vai fazer o registo
     $sql = $db->prepare(" INSERT INTO `user` (`name`,`username`,`password`) VALUES (:name,:user,:pass)");
     
+    //fazemos um "bind" nos parametros:name,user,pass que é onde vai ser enviado a informação, isto para evitar mysql injection
     $sql->bindParam(':name', $name);
     $sql->bindParam(':user', $user);
     $sql->bindParam(':pass', $hashed_pass);
-    
+
+    //faz execute da query que preparamos anteriormente 
     $sql->execute();
     
 }
