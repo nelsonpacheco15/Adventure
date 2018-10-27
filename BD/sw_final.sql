c61d9d2b779c461e17906529dde38034703bc809
Cactusforlife <miguelsardinhapimentel@hotmail.com>
2018-10-27 16:28:11 +0000

    update on DB

--- BD/sw_final.sql
+++ BD/sw_final.sql
@@ -116,6 +116,7 @@ CREATE TABLE IF NOT EXISTS `reservation` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`idUser`)
