41a3a1007c2562184102f0415c57b76e044dfb48
Cactusforlife <Miguelsardinhapimentel@hotmail.com>
2018-11-01 22:36:49 -0100

    fixed a mistake on DB

--- BD/sw_final.sql
+++ BD/sw_final.sql
@@ -19,7 +19,10 @@ SET time_zone = "+00:00";
 --
 -- Database: `sw_final`
 --
-
+DROP DATABASE IF EXISTS `sw_final`;
+CREATE DATABASE
+IF NOT EXISTS `sw_final`;
+USE `sw_final`;
 -- --------------------------------------------------------
 
 --
